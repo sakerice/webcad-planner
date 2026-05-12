48d81e0de1bce6707872c3425ba4db4bd02d5ca9
Naritoshi Iwano <cwq06831@gmail.com>
2026-05-12 23:36:13 +0900

    chore: add build.sh to copy only deployable files to dist/

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+rm -rf dist
+mkdir -p dist/assets/env dist/assets/textures dist/assets/models
+cp index.html dist/
+cp -r assets/. dist/assets/
+echo "Build complete: dist/"
+ls -lh dist/index.html
+du -sh dist/
